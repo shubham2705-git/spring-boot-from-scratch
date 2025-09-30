1570bcc448fbf05b6f00536b139f1c0744f8de58
Shubham Ranjan <gkr74226@gmail.com>
2025-09-30 14:00:39 +0530

    JavaBasedConfigurations..

--- src/SpringProgram4JavaConfig/main/Main.java
+++ src/SpringProgram4JavaConfig/main/Main.java
@@ -1,13 +1,16 @@
 package SpringProgram4JavaConfig.main;
 import SpringProgram4JavaConfig.beans.Student;
 
+import SpringProgram4JavaConfig.resources.SpringConfigFile;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext();
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
         Student std = (Student) context.getBean("stdObj");
+        std.setName("Deepesh");
+        std.setRollno(222);
         std.display();
 
     }
